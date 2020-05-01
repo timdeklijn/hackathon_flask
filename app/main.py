69b6454333dce8a6c4e74c84cb80bd2ab000018d
timdeklijn@gmail.com <200zes1987>
2020-05-01 13:27:13 +0200

    Working Form

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from wtforms import StringField, SubmitField
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
+names = []
+
 class InputForm(FlaskForm):
     name = StringField("Input Your name")
     submit = SubmitField("Submit Name")
@@ -13,8 +15,8 @@ class InputForm(FlaskForm):
 def index():
     form = InputForm()
     if request.method == "POST":
-        print(request.form)
-    return render_template("index.html", form=form)
+        names.append(request.form.get("name"))
+    return render_template("index.html", form=form, names=names)
 
 
 if __name__ == "__main__":
