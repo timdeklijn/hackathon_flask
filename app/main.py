9c7414da4e0599195d2cef82731b852e3bd93edf
timdeklijn@gmail.com <200zes1987>
2020-05-01 13:06:10 +0200

    Added templates

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "YOLO!!"
+def index():
+    name = "Tim"
+    return render_template("index.html", name=name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5000)
