3f808a45d42402a03797adfd2a255d681ce56bc6
timdeklijn@gmail.com <200zes1987>
2020-05-01 15:03:39 +0200

    Added styles

--- app/main.py
+++ app/main.py
@@ -11,6 +11,12 @@ app.config["SECRET_KEY"] = "secret"
 model = load(open(os.path.join("model.pk"), "rb"))
 le = load(open(os.path.join("le.pk"), "rb"))
 
+images = {
+    "Setosa": "https://www.fleurproshop.com/media/img/pics/original/fleur/product/IRISSET01_G998_01.JPG?type=resize&w=610",
+    "Versicolor": "https://daylily-phlox.eu/wp-content/uploads/2016/08/Iris-versicolor-1.jpg",
+    "Virginica": "https://i.pinimg.com/originals/82/81/b9/8281b9a303d5c935cfebf2fac56d22c2.jpg"
+}
+
 
 class IrisForm(FlaskForm):
     sepal_length = FloatField("Sepal Length")
@@ -29,8 +35,8 @@ def index():
         sep_w = request.form.get("sepal_width")
         pet_l = request.form.get("petal_length")
         pet_w = request.form.get("petal_width")
-        variety = le.inverse_transform(model.predict([[sep_l, sep_w, pet_l, pet_w]]))
-    return render_template("index.html", form=form, variety=variety)
+        variety = le.inverse_transform(model.predict([[sep_l, sep_w, pet_l, pet_w]]))[0]
+    return render_template("index.html", form=form, variety=variety, image_link=images[variety])
 
 
 if __name__ == "__main__":
