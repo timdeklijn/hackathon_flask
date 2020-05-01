d58b7a08c1b0d5ce0d29f829b4f9949fb5659c93
timdeklijn@gmail.com <200zes1987>
2020-05-01 14:26:41 +0200

    Added model(s)

--- app/main.py
+++ app/main.py
@@ -2,32 +2,35 @@ import os
 
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FloatField
-import pickle
+from joblib import load
+from wtforms import SubmitField, FloatField
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"
 
-names = []
-
-model = pickle.load(open(os.path.join("model.pk"), 'rb'))
+model = load(open(os.path.join("model.pk"), "rb"))
+le = load(open(os.path.join("le.pk"), "rb"))
 
 
 class IrisForm(FlaskForm):
-    sepal_length = FloatField("Sepal Lenght")
-    sepal_width = FloatField("sepal_width")
-    petal_length = FloatField("petal_length")
-    petal_width = FloatField("petal_width")
+    sepal_length = FloatField("Sepal Length")
+    sepal_width = FloatField("Sepal Width")
+    petal_length = FloatField("Petal Length")
+    petal_width = FloatField("Petal Width")
     submit = SubmitField("Submit Measured Flower")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    print(model)
+    variety = None
     form = IrisForm()
     if request.method == "POST":
-        names.append(request.form.get("name"))
-    return render_template("index.html", form=form, names=names)
+        sep_l = request.form.get("sepal_length")
+        sep_w = request.form.get("sepal_width")
+        pet_l = request.form.get("petal_length")
+        pet_w = request.form.get("petal_width")
+        variety = le.inverse_transform(model.predict([[sep_l, sep_w, pet_l, pet_w]]))
+    return render_template("index.html", form=form, variety=variety)
 
 
 if __name__ == "__main__":
--- train_model.py
+++ train_model.py
@@ -1,19 +1,31 @@
+from joblib import dump
 import pandas as pd
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import SVC
 
 if __name__ == "__main__":
     df = pd.read_csv("iris.csv")
     y = df.loc[:, "variety"]
     X = df.drop(labels="variety", axis=1)
+    le = LabelEncoder()
+    le.fit(y)
+    y = le.transform(y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+
+    print(X_train, y_train)
 
     clf = SVC(gamma='auto')
     clf.fit(X_train, y_train)
+    print(X_test)
     y_pred = clf.predict(X_test)
 
+    print(y_test, y_pred)
+
     print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-    pickle.dump(clf, open("model.pk", 'wb'))
+    dump(clf, open("model.pk", 'wb'))
+    dump(le, open("le.pk", "wb"))
     print("Model Saved")
+
+    print(clf.predict([[5.0, 3.4, 1.6, 0.4]]))
